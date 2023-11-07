c537cd4ec535992e30b25f03cc617c0f8eadbcda
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-11-07 19:13:35 -0300

    Revert "Delete Padroes de Projeto de Sistemas/src/Observer directory"
    This reverts commit c5e7b34510048312c84698db51d22ba0aa051999.

---
+++ Padroes de Projeto de Sistemas/src/Observer/MonitorEstufa.java
@@ -0,0 +1,23 @@
+package Observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MonitorEstufa implements Observer {
+    private String nome;
+
+    public MonitorEstufa(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void update(Observable observado, Object arg) {
+        if (observado instanceof SensorTemperatura) {
+            SensorTemperatura sensor = (SensorTemperatura) observado;
+            System.out.println("Monitor " + nome + " - Temperatura atual: " + sensor.getTemperatura() + "°C");
+        }
+    }
+
+
+}
+
